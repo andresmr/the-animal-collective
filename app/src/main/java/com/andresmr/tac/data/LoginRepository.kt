2c6f5d54430725516223ee0014ff80c7376a6719
andresmr <andres@andresmr.com>
2022-04-26 16:40:49 +0200

    Refactor repository as suspend function

--- app/src/main/java/com/andresmr/tac/data/LoginRepository.kt
+++ app/src/main/java/com/andresmr/tac/data/LoginRepository.kt
@@ -1,15 +1,17 @@
 package com.andresmr.tac.data
 
-import org.hisp.dhis.android.core.D2Manager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.hisp.dhis.android.core.D2
 import org.hisp.dhis.android.core.user.User
 
-class LoginRepository {
-    private val d2 = D2Manager.getD2()
+class LoginRepository(private val d2: D2) {
 
-
-    fun login(): User? = d2.userModule().blockingLogIn(
-        "admin",
-        "district",
-        "http://10.0.2.2:8080"
-    )
+    suspend fun login(): User? = withContext(Dispatchers.IO) {
+        d2.userModule().blockingLogIn(
+            "admin",
+            "district",
+            "http://10.0.2.2:8080"
+        )
+    }
 }
--- app/src/main/java/com/andresmr/tac/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/andresmr/tac/ui/login/LoginViewModel.kt
@@ -5,34 +5,41 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.andresmr.tac.data.LoginRepository
 import com.andresmr.tac.utils.ErrorMessage
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import org.hisp.dhis.android.core.D2Manager
 import org.hisp.dhis.android.core.maintenance.D2Error
 
 class LoginViewModel : ViewModel() {
 
-    private val loginRepository = LoginRepository()
+    private val loginRepository = LoginRepository(D2Manager.getD2())
     val uiState = MutableStateFlow(LoginViewModelState(isLoading = true))
 
     init {
-        viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                try {
-                    uiState.value = loginRepository.login()?.let {
-                        LoginViewModelState(false)
-                    } ?: LoginViewModelState(false, ErrorMessage("Something went wrong..."))
-                } catch (e: Exception) {
-                    if (e.cause is D2Error) {
-                        val d2Error = e.cause as D2Error
-                        Log.e(
-                            "LOGIN",
-                            d2Error.errorComponent()
-                                .toString() + " " + d2Error.httpErrorCode() + " " + d2Error.errorCode()
-                        )
-                    }
+        launchDataLoad {
+            uiState.value = loginRepository.login()?.let {
+                LoginViewModelState(false)
+            } ?: LoginViewModelState(false, ErrorMessage("Something went wrong..."))
+        }
+    }
+
+    private fun launchDataLoad(block: suspend () -> Unit): Job {
+        return viewModelScope.launch {
+            try {
+//                _spinner.value = true
+                block()
+            } catch (error: Exception) {
+//                _snackBar.value = error.message
+                if (error.cause is D2Error) {
+                    val d2Error = error.cause as D2Error
+                    val message = d2Error.errorComponent().toString() + " " +
+                        d2Error.httpErrorCode() + " " + d2Error.errorCode()
+                    Log.e("LOGIN", message)
                 }
+
+            } finally {
+//                _spinner.value = false
             }
         }
     }
